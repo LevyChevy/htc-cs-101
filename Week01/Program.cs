b78eae842ae964958639bdfe0d32ba9b6e7151b2
User <User@DEV-022782.OnBase.net>
2019-02-12 19:17:11 -0500

    Created an about me program.

--- Week01/Program.cs
+++ Week01/Program.cs
@@ -7,7 +7,31 @@ namespace Week01
         static void Main(string[] args)
         {
             // say Hello World here
-            
+            Console.WriteLine("Hello World");
+
+            //About me
+            string myName = "Levi";
+            int myAge = 32;
+            string favColor = "Blue";
+            string myHobby = "hi";
+            bool isOldEnoughToDrive = false;
+            double myHeight = 5.82;
+            char firstInitial = 'l';
+
+            Console.WriteLine("My name is " + myName);
+            //Console.WriteLine("My age is {0}", myAge);
+            Console.WriteLine("My Age is " + myAge);
+            Console.WriteLine("My favorite color is " + favColor);
+            Console.WriteLine("My favorite hobby is" + myHobby);
+            Console.WriteLine("My height is" + myHeight);
+            Console.WriteLine("The first initial of my name is" + firstInitial);
+            Console.WriteLine("I am old enough to dive " + isOldEnoughToDrive);
+
+
+
+
+
+
         }
     }
 }
