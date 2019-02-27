eb9fee102fc3c9d97e628c0395b8488fa5ce00de
User <User@DEV-022774.OnBase.net>
2019-02-26 19:25:42 -0500

    Week 4 codeAlong

---
+++ Week04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool snowing = true;
+            //bool haveCarrot = true;
+           //int temp = 40;
+            //string clothes = "outfit";
+            //string name = "Lucas";
+
+            //if ((snowing && temp <= 40) && (name != "Bob" || haveCarrot)){
+                //Console.WriteLine("I'm a snowman");
+
+                if (true || false){
+                    Console.WriteLine("True");
+                }
+                else {
+                    Console.WriteLine("False");
+                }
+
+        
+        }
+    }
+}
