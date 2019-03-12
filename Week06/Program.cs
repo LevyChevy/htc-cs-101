417bfe1c829182cd619387f6a7db79c2d0525290
User <User@DEV-022776.OnBase.net>
2019-03-12 19:26:47 -0400

    Did stuff

---
+++ Week06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Week06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Enter a number: ");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+            Console.Writeline(factorial(userInput));
+        }
+
+        public static int factorial(int number)
+        {
+            int total = 1;
+            while (number > 0)
+            {
+                total *= number;
+                number--;
+            }
+            return total;
+        }
+    }
+}
---
+++ Week06/Week06 challenges/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week06_challenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 100){
+                 if(x % 2 == 0)
+            {
+                Console.ReadLine(x);
+            }
+                x++;
+            }return x;
+        }
+    }
+}
